264502abc01b56928ceaa3d5bcf54fa4b7cd2516
Nil <nilotpal17@gmail.com>
2025-01-20 14:02:14 +0530

    Updates

--- netlify/functions/express.js
+++
@@ -1,4 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("../../index"); // Import your express app
-
-module.exports.handler = serverless(app); // Wrap your app using serverless-http
